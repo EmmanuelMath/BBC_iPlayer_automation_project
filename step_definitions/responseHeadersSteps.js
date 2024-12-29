9b40e20a7b710946133ad6129181db7e750b7004
mannyy Manny <emmamthar@gmail.com>
2024-12-29 21:48:01 +0000

    refactored responseHeadersSteps file

--- step_definitions/responseHeadersSteps.js
+++ step_definitions/responseHeadersSteps.js
@@ -5,22 +5,21 @@ const feature = loadFeature('./features/responseHeaders.feature');
 
 defineFeature(feature, (test) => {
   let response;
+  let responseHeader;
 
   test('Scenario 6: Verify presence of Date header', ({ given, then }) => {
     given(/^I send a GET request to "(.*)"$/, async (endpoint) => {
       try {
         response = (await apiClient.get(endpoint));
+        responseHeader = response.headers;
       } catch (error) {
         throw new Error(`API request failed: ${error.message}`);
       };
     });
 
     then(/^the response headers should contain a "(.*)" field$/, async (date) => {
-      const isFieldInHeaderResponse = date in response.headers;
-      expect(isFieldInHeaderResponse).toBeTruthy()
-      expect(response.headers[date]).toBeDefined();
-      expect(response.headers[date]).not.toBeNull();
-      expect(response.headers[date]).not.toBe('');
+      expect(responseHeader).toBeDefined();
+      expect(responseHeader).toHaveProperty(date);
     });
   });
 });
