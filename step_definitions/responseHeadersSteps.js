09b2c0ba965c1971922c3fea75d9520b66b7f5c0
mannyy Manny <emmamthar@gmail.com>
2024-12-29 08:57:24 +0000

    automated tests for response headers scenarios

---
+++ step_definitions/responseHeadersSteps.js
@@ -0,0 +1,18 @@
+const { defineFeature, loadFeature } = require('jest-cucumber');
+const apiClient = require('../utils/apiClient');
+
+const feature = loadFeature('./features/responseHeaders.feature');
+
+defineFeature(feature, (test) => {
+  let response;
+
+  test('Scenario 6: Verify presence of Date header', ({ given, then }) => {
+    given(/^I send a GET request to "(.*)"$/, async (endpoint) => {
+      response = await apiClient.get(endpoint);
+    });
+
+    then(/^the response headers should contain a "(.*)" field$/, async (date) => {
+        expect(response.headers[date]).toBeDefined();
+    });
+  });
+});
